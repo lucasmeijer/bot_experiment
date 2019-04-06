21dbb48abfb6d7483178314f0b39ce323d9935ee
Lucas Meijer <lucas@unity3d.com>
2019-04-06 21:56:56 +0200

    tryme

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace bot_experiment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! "+args[0]);
         }
     }
 }
