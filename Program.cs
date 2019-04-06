e8306a9447499c77df1b430ea4811ad8c345a15e
Lucas Meijer <lucas@unity3d.com>
2019-04-06 21:45:23 +0200

    lets try some dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bot_experiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
