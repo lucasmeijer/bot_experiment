9a6efbb6738a9a259bcd2897c253c32e15b06316
Lucas Meijer <lucas@unity3d.com>
2019-04-07 18:47:09 +0200

    dummy commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Principal;
 using Octokit;
 using Octokit.Internal;
@@ -10,24 +11,40 @@ namespace bot_experiment
     {
         static void Main(string[] args)
         {
-            string token = Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? "none";
+            string token = Environment.GetEnvironmentVariable("GITHUB_TOKEN") ?? "2ef5160ac642a8c562ed767787ea2b586225cbc9";
             Console.WriteLine("GitHub token first few chars: "+token.Substring(0,3));
             Console.WriteLine($"PullRequest number is {Environment.GetEnvironmentVariable("SYSTEM_PULLREQUEST_PULLREQUESTNUMBER") ?? "nonumber"}");
+
+
             
             var credentials = new InMemoryCredentialStore(new Credentials(token));
             var client = new ObservableGitHubClient(new ProductHeaderValue("ophion"), credentials);
             
-            var pr = client.PullRequest.Get("lucasmeijer", "bot_experiment", 1).Subscribe(a =>
+            var pr = client.PullRequest.Get("lucasmeijer", "bot_experiment", 2).Subscribe(async a =>
                 {
-                    Console.WriteLine("##vso[task.setvariable variable=PullRequestBase;isOutput=true]d1294391ab66438a39a613673f01ee190b96787b");
-                    Console.WriteLine($"Base is {a.Base.Sha}");
-                    Console.WriteLine($"Head is {a.Head.Sha}");
+                    
+                    if (a.Mergeable != true)
+                        throw new Exception("This PR is not reporting itself as mergable");
+
+                    var mergeCommit = a.MergeCommitSha;
+                    var queryCommit = client.Repository.Commit.Get("lucasmeijer", "bot_experiment", mergeCommit).Subscribe(commit =>
+                    {
+                        var referenceCommit = commit.Parents.First(p => p != a.Head);
+                        Console.WriteLine($"##vso[task.setvariable variable=PullRequestBase;isOutput=true]{referenceCommit.Sha}");
+                        Console.WriteLine($"##vso[task.setvariable variable=TestMergeCommit;isOutput=true]{commit.Sha}");
+
+                        Console.WriteLine($"referenceCommit is {referenceCommit.Sha}");
+                        Console.WriteLine($"testMergeCommit is {commit.Sha}");
+                        Console.WriteLine($"Base is {a.Base.Sha}");
+                        Console.WriteLine($"Head is {a.Head.Sha}");
+                        Console.WriteLine($"Title is: {a.Title}");
+                    },
+                        
+                        onCompleted: () => Environment.Exit(0)
+                        );
                     
                     
-                    
-                    Console.WriteLine($"Title is: {a.Title}");
                 },
-                onCompleted: () => Environment.Exit(0),
                 onError:(e) =>
                 {
                    Console.WriteLine("ERROR!!! " + e);
