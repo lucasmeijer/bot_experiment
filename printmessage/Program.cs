6d63fd612e341d3c704d607d19e18190a28ed3ee
Lucas Meijer <lucas@unity3d.com>
2019-04-09 16:22:32 +0200

    only warning

--- printmessage/Program.cs
+++ printmessage/Program.cs
@@ -8,7 +8,6 @@ namespace printmessage
         static void Main(string[] args)
         {
             Console.WriteLine("##vso[task.logissue type=warning]BuildSize report has reached a big limit");
-            Console.WriteLine("##vso[task.logissue type=error]We found a really big problem");
             Console.WriteLine($"##vso[task.uploadsummary]{Directory.GetCurrentDirectory()}/testsummary.md");
         }
     }
