477b1e1e62678095fe6e81aed7c0842b362ce4c4
Lucas Meijer <lucas@unity3d.com>
2019-04-09 15:46:09 +0200

    test this

--- printmessage/Program.cs
+++ printmessage/Program.cs
@@ -7,6 +7,8 @@ namespace printmessage
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("##vso[task.logissue type=warning]BuildSize report has reached a big limit")
+            Console.WriteLine("##vso[task.logissue type=error]We found a really big problem")
             Console.WriteLine($"##vso[task.uploadsummary]{Directory.GetCurrentDirectory()}/testsummary.md");
         }
     }
